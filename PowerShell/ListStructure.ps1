35a9683314c1cafea3c5b2b1f4a023a249b03b00
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-28 14:31:56 +0530

    Create ListStructure.ps1 (#3)

---
+++ PowerShell/ListStructure.ps1
@@ -0,0 +1,32 @@
+﻿# Usage: .\ListStructure.ps1 -Path "C:\Project1"
+# Usage: PowerShell.exe -ExecutionPolicy Bypass -File "C:\Tools\ListStructure.ps1" -Path "C:\GitHub\learn-reactjs-in-2023\taskmanager" -ExcludedFolders ".next", "node_modules"
+# Usage: PowerShell.exe -ExecutionPolicy Bypass -File "D:\TSA\travels-flask-azoai-api\scripts\ListStructure.ps1" -Path "D:\TSA\travels-flask-azoai-api"
+param (
+    [string]$Path = (Get-Location),
+    [string[]]$ExcludedFolders = @("node_modules", ".next", "bin", "obj", "__pycache__", ".venv")
+)
+
+function List-DirectoryStructure {
+    param (
+        [string]$Path,
+        [string]$Indent = ''
+    )
+    
+    $items = Get-ChildItem $Path
+
+    foreach ($item in $items) {
+        # Check if the current item should be excluded
+        if ($item.Name -in $ExcludedFolders) {
+            continue  # Skip this folder
+        }
+
+        Write-Host "$Indent- $($item.Name)"
+
+        if ($item.PSIsContainer) {
+            List-DirectoryStructure -Path $item.FullName -Indent "$Indent  "
+        }
+    }
+}
+
+Write-Host $Path
+List-DirectoryStructure -Path $Path
